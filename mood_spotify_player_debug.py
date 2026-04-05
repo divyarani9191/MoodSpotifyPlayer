736ae149aa707f7320ac25d742c33f6d71c76501
divyarani9191 <divyaraninaw@gmail.com>
2026-04-05 10:13:18 +0530

    voice.py

--- mood_spotify_player_debug.py
+++ mood_spotify_player_debug.py
@@ -35,14 +35,14 @@ def speak(text):
 
 # ===================== SPOTIFY =====================
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="63ae557f57cd4e31aeb8bcc0db1673d8",
-    client_secret="93cc866850954c41a38d0f5b16925a03",
+    client_id="  ",
+    client_secret=" ",
     redirect_uri="http://127.0.0.1:8888/callback",
     scope="user-read-playback-state,user-modify-playback-state"
 ))
 
 # ===================== MONGODB =====================
-uri = "mongodb+srv://emoheal_user:Emoheal123@cluster0.hwezm4z.mongodb.net/?appName=Cluster0"
+uri = " "
 client = MongoClient(uri)
 db = client["emohealDB"]
 voice_collection = db["voice_emotions"]
